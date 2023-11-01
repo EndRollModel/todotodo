ff10186d7f1b944952b6763eb36c20252b4e0d03
EndRollModel <51425629+EndRollModel@users.noreply.github.com>
2023-11-01 17:53:00 +0800

    feat : index.html : 新增readerer的測試功能 preload.js : 測試contextBridge renderer.js : 測試 todo.md : 新增了新的功能項目

---
+++ renderer.js
@@ -0,0 +1,5 @@
+// document.getElementById('myButton').addEventListener('click', () => {
+//     ipcRenderer.send('button-clicked');
+// });
+
+document.body.textContent = window.versions('./todo.md')
